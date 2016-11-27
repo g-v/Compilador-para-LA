75d94d2e46c0e819f8ebc8a2a87b83220a15856b
realm087 <bc.greco@hotmail.com>
2016-11-27 21:27:40 -0200

    começando o semantico feelsgudman

--- src/trabalho1/AnalizadorSemantico.java
+++ src/trabalho1/AnalizadorSemantico.java
@@ -15,56 +15,85 @@ public class AnalizadorSemantico extends LABaseVisitor<Void>{
     TabelaDeSimbolos_VAR tdsVAR;
     TabelaDeSimbolos_TIPOS tdsTIPOS;
     TabelaDeSimbolos_STRCT tdsSTRCT;
+
+    //tmp
     
+    int tipo;
+    
+    //tmp
+
     @Override
     public Void visitDeclaracao_local(LAParser.Declaracao_localContext ctx) {
-        if(ctx.variavel() != null)
+        if(ctx.dclLocalConst != null)
+        {
+            EntradaTS_TIPO etds = tdsTIPOS.verificar(ctx.tipo().getText());
+            if(ctx.tipo() == null)
+            {
+                //erro tipo nao declarado
+            }else
+            {
+                if(tdsVAR.verificar(ctx.IDENT().getText()) != null)
+                {
+                    //erro, variavel ja declarada
+                }else
+                    tdsVAR.inserir(ctx.IDENT().getText(), etds.valor, 1);
+            }
+        }else if(ctx.dclLocalTipo != null)
+        {
+            
+        }else // variavel
         {
             visitVariavel(ctx.variavel());
         }
-        else if(ctx.dclLocalConst != null)
-        {
-            EntradaTS_TIPO tipo = tdsTIPOS.verificar(ctx.tipo_basico().getText());
-            if(tipo == null)
-                //erro
-            
-            if(tdsVAR.verificar(ctx.IDENT().getText()) == null)
-                //erro
-                
-            tdsVAR.inserir(ctx.IDENT().getText(), tipo.valor, 1);
-        }
-        else if(ctx.dclLocalTipo != null)
-        {
-            if(ctx.tipo().registro() != null)
-            {
-                String nome_STRCT = ctx.dclLocalTipo.getText();
-                tdsSTRCT.inserir(nome_STRCT);
-                LAParser.RegistroContext reg = ctx.tipo().registro();
-                
-                String tipoVar = reg.variavel().tipo().tipo_estendido().tipo_basico_ident().getText();
-                EntradaTS_TIPO etds = tdsTIPOS.verificar(tipoVar);
-                if(etds == null)
-                {
-                    //erro
-                }
-                   
-                String nomeVar = reg.variavel().IDENT().getText();
-                
-                if(tdsSTRCT.verificarVarEmSTRCT(nome_STRCT, nomeVar) == null)
-                    //erro
-                    
-                tdsSTRCT.inserirVarEmSTRCT(nome_STRCT, nomeVar, 
-                        etds.valor, Integer.parseInt(reg.variavel().dimensao().getText()));
-                
-            }
-            else if(ctx.tipo().tipo_estendido() != null)
-            {
-                
-            }
-        }
-        
-        return Void;
+        return null;
     }
+
+    @Override
+    public Void visitVariavel(LAParser.VariavelContext ctx) {
+        
+        EntradaTS_TIPO etds = tdsTIPOS.verificar(ctx.tipo().getText());
+        if(etds == null)
+        {
+            //erro tipo nao existe
+        }else
+        {
+           tipo = etds.valor;
+           if(tdsVAR.verificar(ctx.IDENT().getText()) != null)
+           {
+               //erro, variavel ja declarada
+           }else
+           {    
+               tdsVAR.inserir(ctx.IDENT().getText(), tipo, Integer.parseInt(ctx.dimensao().getText()));
+               if(ctx.mais_var() != null)
+               {
+                   visitMais_var(ctx.mais_var());
+               }
+           }
+        }
+
+        
+        return null;
+    }
+    
+    
+    
+    @Override
+    public Void visitMais_var(LAParser.Mais_varContext ctx) {
+        if(tdsVAR.verificar(ctx.IDENT().getText()) != null)
+        {
+            //erro, variavel ja declarada
+        }else
+        {
+            tdsVAR.inserir(ctx.IDENT().getText(), tipo, Integer.parseInt(ctx.dimensao().getText()));
+                if(ctx.mais_var() != null)
+                    visitMais_var(ctx.mais_var());
+        
+        }
+  
+        return null;
+    }
+    
+    
 
     
     
